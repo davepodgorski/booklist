0fada171d72dee05a1460d34fb863e0f388a7476
davepodgorski <davepodgorski@gmail.com>
2018-12-18 15:40:54 -0500

    booklist working

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,43 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
+      respond_to do |format|
+        format.html
+        format.js
+        format.xml { render xml: @book }
+        format.text
+        format.csv do
+          render plain: Book.generate_csv(@books)
+          end
+          format.json do
+            render json: @books
+          end
+    end
+
+  end
 
 
-end
+  def show
+
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new
+  end
+
+      def edit
+          @book = Book.find(params[:id])
+      end
+
+      def update
+
+      end
+
+      def destroy
+
+      end
+  end
